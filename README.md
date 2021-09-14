7e8defae05032961bdeef84d75452c500f03c4e2
Elliot De La Cruz <20199039@itla.edu.do>
2021-09-14 16:01:36 -0400

    Este es mi primer comnit

---
+++ README.md
@@ -0,0 +1,2 @@
+Esta es la tarea # 1 de Elliot De La Cruz, esta es la captura de pantalla:
+![Mi captura de pantalla](mitarea.png)
---
+++ main.js
@@ -0,0 +1,6 @@
+alert('Para continuar haga clic en ACEPTAR');
+var nombre = "Elliot De La Cruz Caro"
+var altura = "5,6"
+/*document.write("Este es mi nombre: "+nombre," y esta es mi altura: "+ altura);*/
+
+var datos = document.getElementById("datos");
